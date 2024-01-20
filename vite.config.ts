41ffe446fbc644a27a62c9804967d6eb4b7d4eb2
Aman Ullah <amanix007@gmail.com>
2024-01-20 21:04:23 +0600

    config test with netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/quiz-app-user-admin-and-general-ts/', 
+  // base: '/quiz-app-user-admin-and-general-ts/', 
   
 })
