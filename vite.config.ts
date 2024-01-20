6e490911db4a2e3bcaf71596b69929d00b449cc5
Aman Ullah <amanix007@gmail.com>
2024-01-20 20:46:31 +0600

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quiz-app-user-admin-and-general-ts/', // Replace with your repository name
+  
 })
